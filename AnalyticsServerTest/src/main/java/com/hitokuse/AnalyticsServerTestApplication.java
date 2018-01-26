22a3a1890c5f111987402d40c301affa4510e39b
Administrator <Administrator@WIN-0RTHSPRLP6F>
2018-01-26 20:49:01 +0900

    Initialize project

---
+++ AnalyticsServerTest/src/main/java/com/hitokuse/AnalyticsServerTestApplication.java
@@ -0,0 +1,12 @@
+package com.hitokuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsServerTestApplication.class, args);
+	}
+}
